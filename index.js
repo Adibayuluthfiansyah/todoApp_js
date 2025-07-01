4d11cbf6d301086bb5e7a1565e29cf8421bd8a02
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-07-02 01:49:56 +0700

    fix logic js

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const todoInput = document.getElementById("todoInput");
 const mainList = document.getElementById("mainList");
 const addBtn = document.getElementById("addBtn");
 
-addBtn.addEventListener("click", () => {
-  console.log("Button Di klik");
+addBtn.addEventListener("click", function addTask() {
+  if (todoInput.value === "") {
+    alert("Anda belum masukkan apapun");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = todoInput.value;
+    mainList.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "❌";
+    li.appendChild(span);
+  }
+  todoInput.value = "";
+
+  mainList.addEventListener(
+    "click",
+    function (e) {
+      if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+      } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+      }
+    },
+    false
+  );
 });
