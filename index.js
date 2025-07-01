cbf026ed4ee30d1cc7c198f71d15bd6204606047
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-07-02 01:22:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const todoInput = document.getElementById("todoInput");
+const mainList = document.getElementById("mainList");
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", () => {
+  console.log("Button Di klik");
+});
